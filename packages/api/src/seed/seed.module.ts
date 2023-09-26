2108bb1763268cef6632c6b1d7de3064c507a24b
VanderschuerenMaximilian <91435601+VanderschuerenMaximilian@users.noreply.github.com>
2023-09-26 16:52:53 +0200

    feat: seeding werknemers

--- packages/api/src/seed/seed.module.ts
+++ packages/api/src/seed/seed.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommandModule } from 'nestjs-command';
 import { WerknemerModule } from 'src/werknemer/werknemer.module';
 import { SeedService } from './seed.service';
+import { DatabaseSeedCommand } from './seed.command';
 
 @Module({
     imports: [WerknemerModule, CommandModule],
-    providers: [SeedService],
+    providers: [DatabaseSeedCommand ,SeedService],
 })
 export class SeedModule {}
--- packages/api/src/seed/seed.service.ts
+++ packages/api/src/seed/seed.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { WerknemerService } from 'src/werknemer/werknemer.service';
+import { Werknemer } from 'src/werknemer/entities/werknemer.entity';
+
+import * as werknemers from './data/werknemers.json';
 
 @Injectable()
-export class SeedService {}
+export class SeedService {
+    constructor(
+        private werknemerService: WerknemerService,
+    ) {}
+
+    async addWerknemersFromJson(): Promise<Werknemer[]> {
+        let theWerknemers: Werknemer[] = [];
+
+        for (let werknemer of werknemers) {
+            const newWerknemer = new Werknemer();
+            newWerknemer.firstname = werknemer.firstname;
+            newWerknemer.lastname = werknemer.lastname;
+            newWerknemer.email = werknemer.email;
+
+            theWerknemers.push(newWerknemer)
+        }
+
+        return await this.werknemerService.saveAll(theWerknemers);
+    }
+
+    async deleteAllWerknemers(): Promise<void> {
+        await this.werknemerService.truncate();
+    }
+}
