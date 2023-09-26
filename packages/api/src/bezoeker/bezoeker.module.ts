29733b21e89115ddcb0a69a0a0594ab64c0f2b93
VantommeJaron <jaron.vantomme@student.howest.be>
2023-09-26 16:29:43 +0200

    bezoekers mongo

--- packages/api/src/bezoeker/bezoeker.module.ts
+++ packages/api/src/bezoeker/bezoeker.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BezoekerService } from './bezoeker.service';
 import { BezoekerResolver } from './bezoeker.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bezoeker } from './entities/bezoeker.entity';
 
 @Module({
   providers: [BezoekerResolver, BezoekerService],
+  imports: [TypeOrmModule.forFeature([Bezoeker])],
+  exports: [BezoekerService],
 })
 export class BezoekerModule {}
--- packages/api/src/bezoeker/bezoeker.service.ts
+++ packages/api/src/bezoeker/bezoeker.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBezoekerInput } from './dto/create-bezoeker.input';
 import { UpdateBezoekerInput } from './dto/update-bezoeker.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Bezoeker } from './entities/bezoeker.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BezoekerService {
-  create(createBezoekerInput: CreateBezoekerInput) {
-    return 'This action adds a new bezoeker';
+  constructor(
+    @InjectRepository(Bezoeker)
+    private readonly bezoekerRepository: Repository<Bezoeker>,
+  ) {}
+  
+  create(createBezoekerInput: CreateBezoekerInput): Promise<Bezoeker> {
+    const b = new Bezoeker()
+    b.name = createBezoekerInput.name
+    b.fullname = createBezoekerInput.fullname
+    b.email = createBezoekerInput.email
+    return this.bezoekerRepository.save(b)
   }
 
   findAll() {
-    return `This action returns all bezoeker`;
+    return `This action returns all bezoeker`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} bezoeker`;
+    return `This action returns a #${id} bezoeker`
   }
 
   update(id: number, updateBezoekerInput: UpdateBezoekerInput) {
-    return `This action updates a #${id} bezoeker`;
+    return `This action updates a #${id} bezoeker`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} bezoeker`;
+    return `This action removes a #${id} bezoeker`
   }
 }
