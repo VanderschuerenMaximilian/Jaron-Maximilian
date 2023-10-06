90bcab977272cda20a4640d1a2ba46ba16e26e9d
VantommeJaron <jaron.vantomme@student.howest.be>
2023-10-06 13:39:28 +0200

    fix: dist folder deleted

--- packages/api/dist/seed/seed.command.d.ts
+++
@@ -1,9 +0,0 @@
-import { SeedService } from './seed.service';
-export declare class DatabaseSeedCommand {
-    private readonly seedService;
-    constructor(seedService: SeedService);
-    seedPersons(): Promise<void>;
-    deletePersons(): Promise<void>;
-    seedAlerts(): Promise<void>;
-    deleteAlerts(): Promise<void>;
-}
