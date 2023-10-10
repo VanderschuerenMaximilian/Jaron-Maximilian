6d763c074e8a41761911e4dd1defe6d668f6dd74
VanderschuerenMaximilian <91435601+VanderschuerenMaximilian@users.noreply.github.com>
2023-10-10 14:00:25 +0200

    update: start multi language

---
+++ packages/pwa/src/bootstrap/i18n.ts
@@ -0,0 +1,28 @@
+import { type I18nOptions, createI18n } from 'vue-i18n'
+
+export const SUPPORTED_LOCALES: Record<string, string> = {
+  en: 'English',
+  nl: 'Nederlands',
+  fr: 'Français',
+}
+
+export const DEFAULT_LOCALE = 'en'
+
+export const i18nOptions: I18nOptions = {
+  locale: DEFAULT_LOCALE,
+  fallbackLocale: DEFAULT_LOCALE,
+  legacy: false,
+  messages: {
+    en: {
+      hello: 'hello world',
+    },
+    nl: {
+      hello: 'hallo wereld',
+    },
+    fr: {
+      hello: 'bonjour monde',
+    },
+  },
+}
+
+export const i18n = createI18n(i18nOptions)
